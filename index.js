c8fc603bdee5f89b003ac2d565702c9edf1c9422
pizzamath <slangows@purdue.edu>
2018-04-09 15:20:09 -0400

    updatedLogin

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(express.static('public'))
 
+app.route('/profiles/:userId').get(function(req, res){
+    console.log(req.params['userId']);
+});
+
 //start server
 app.listen(8000, function(){
     console.log("Server started"); 
