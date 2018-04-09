613f4ad9048013f3950ac10a661a8bcb203a99cf
pizzamath <slangows@purdue.edu>
2018-04-09 16:00:53 -0400

    added redirect to login screen

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.route('/profiles/:userId').get(function(req, res){
     console.log(req.params['userId']);
 });
 
+app.all("/", (req, res) => {
+    res.redirect(301, "/login.html");
+});
+
 //start server
 app.listen(8000, function(){
     console.log("Server started"); 
