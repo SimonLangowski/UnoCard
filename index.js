a8b33999a319244f03085c824b8714a6452b6d16
pizzamath <slangows@purdue.edu>
2018-04-09 20:49:56 -0400

    Added cookies and javascript

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 //dependencies
 const express = require('express');
 
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
 //get app
 const app = express();
+app.use(bodyParser.json());
+/*var connection = mysql.createConnection({
+    
+});*/ //TODO finish
+
+function queryDatabase(query){
+
+}
+
+function checkUsername(username){
+    
+}
+
+//remove non alphanumeric characters from usernames for safe SQL parsing
+function cleanString(string){
+    return string.replace(/[^a-zA-z0-9]/, '')
+}
+
 
 app.use(express.static('public'))
 
 app.route('/profiles/:userId').get(function(req, res){
     console.log(req.params['userId']);
+});
+
+
+
+app.route('/register').post(function(req, res){
+    var username = req.body.username;
+    var password = res.body.password;
+    //check for username
+    if (checkUsername(username)){
+        
+    }
+    console.log(username);
+    console.log(password);
+    //
+    var test = {
+        status : success,
+        userId : 12490,
+        userAuth : someHashThingy,
+        error : none
+    }
+    res.send(JSON.stringify(test));
 });
 
 app.all("/", (req, res) => {
