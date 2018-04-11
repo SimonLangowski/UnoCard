35ed2b060ff99fd51736d510bb99d2a9f452da9e
pizzamath <slangows@purdue.edu>
2018-04-10 23:41:58 -0400

    trying Angular

---
+++ public/login.js
@@ -0,0 +1,6 @@
+angular.module('loginApp' []).controller('LoginController', function() {
+    var login = this;
+    login.username = {text:'Username'}
+    login.password = {text:'password'}
+    login.type = {text:''}
+}
