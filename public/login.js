215eaba571c0816140dedcecb9c67feb07d06d8e
simonLangowski <slangows@purdue.edu>
2018-04-11 14:53:06 -0400

    Implemented Angular

--- public/login.js
+++ public/login.js
@@ -1,6 +1,39 @@
-angular.module('loginApp' []).controller('LoginController', function() {
-    var login = this;
-    login.username = {text:'Username'}
-    login.password = {text:'password'}
-    login.type = {text:''}
-}
+angular.module('loginApp', [])
+.controller('loginController', ['$scope', '$http', function($scope, $http){
+    $scope.formData = {}; //  empty object to hold input data
+
+    $scope.register = function(){
+        console.log($scope.formData);
+        $http.post('/register', $scope.formData)
+            .then(function(data) {
+                if (data.status === "success"){
+                    //set cookie
+                    document.cookie = "userID=" + data.userID + ";path=/";
+                    document.cookie = "userAuth=" + data.userAuth + ";path=/";
+                    console.log(data);
+                    //redirect
+                    //window.location.href = "/lobby.html";
+                } else {
+                    
+                    console.log(data);
+                }
+                
+            }),
+            (function(data) {
+                console.log('Error: ' + data);
+            });
+            
+    };
+    
+    $scope.signIn = function(){
+        $http.post('/signIn', $scope.formData)
+            .then(function(data) {
+                console.log(data);
+             
+            }),
+            (function(data) {
+                console.log('Error: ' + data);
+            });
+        
+    }
+}]);
