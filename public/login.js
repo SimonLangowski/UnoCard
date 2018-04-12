d6e3e172a16cbbefe09c31c21c2860016f83713a
simonLangowski <slangows@purdue.edu>
2018-04-11 21:06:58 -0400

    Working on game page

--- public/login.js
+++ public/login.js
@@ -2,13 +2,21 @@ angular.module('loginApp', [])
 .controller('loginController', ['$scope', '$http', function($scope, $http){
     $scope.formData = {}; //  empty object to hold input data
     $scope.formData.isError = false; //set to no error message
+    $scope.formData.isSuccess = false;
     $scope.register = function(){
         
             $http.post('/register', $scope.formData)
             .then(function(response) {
-                console.log(response.data);
-                $scope.formData.isError = true;
-                $scope.formData.error = response.data.error;
+                if (response.data.status === "success"){
+                    $scope.formData.isError = false;
+                    $scope.formData.isSuccess = true;
+                    $scope.formData.message = response.data.message;
+                } else {
+                    console.log(response.data);
+                    $scope.formData.isError = true;
+                    $scope.formData.isSuccess = false;
+                    $scope.formData.message = response.data.error;
+                }
             }),
             (function(response) {
                 console.log('Error: ' + response);
@@ -21,14 +29,17 @@ angular.module('loginApp', [])
             $http.post('/signIn', $scope.formData)
             .then(function(response) {
                 if (response.data.status === "success"){
-                    $scope.formData.isError=false;
+                    $scope.formData.isError =false;
+                    $scope.formData.isSuccess = true;
+                    $scope.formData.message = response.data.message;
                     //set cookie
-                    document.cookie = "userID=" + response.data.userID + ";path=/";
+                    document.cookie = "USER_ID=" + response.data.userID + ";path=/";
                     //redirect
-                    //window.location.href = "/lobby.html";
+                    window.location.href = "/lobby.html";
                 } else {
                     $scope.formData.isError = true;
-                    $scope.formData.error = response.data.error;
+                    $scope.formData.isSuccess = false;
+                    $scope.formData.message = response.data.error;
                 }
             }),
             (function(data) {
