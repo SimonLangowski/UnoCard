d16e2a6048b5ad009452a2109eb41d7707c81e5e
yeh29 <yeh29@purdue.edu>
2018-04-15 18:05:53 -0400

    Lobby Create, Info, Join, Leave, Start

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ function signOutCheck(userID, res) {
         } else {
             var response = {
                 status: 'failure',
-                error: 'User Doesn\'t Exist or Not Signed In'
+                error: 'User Not Signed In'
             };
             console.log(JSON.stringify(response));
             res.send(JSON.stringify(response));
@@ -116,7 +116,7 @@ function createLobbyCheck(userID, res) {
     var usersRef = database.ref("users");
     userRef.once("value").then(function (snapshot) {
         if (snapshot.child(userID).exists() && snapshot.child(userID).child("signedIn").val() == 'true' &&
-            snapshot.child(userID).child("inLobby").val() == 'false') { 
+            snapshot.child(userID).child("inLobby").val() == 'false') {
             //Create New Lobby
             var currentUserRef = database.ref("users/" + userID);
             currentUserRef.update({
@@ -128,23 +128,37 @@ function createLobbyCheck(userID, res) {
                 while (snapshot.child(generatedID).exists() == true) {
                     generatedID = createdGameID();
                 }
+                var players = [];
+                players.push(userID);
                 gamesRef.update({
                     [generatedID]: {
-                        //TODO: LOBBY ELEMENTS
+                        names: players,
+                        gameID: generatedID,
+                        isStarted: false,
+                        partyLeader: userID,
+                        stateID: 1
                     }
                 });
                 var response = {
                     status: 'success',
-                    message: 'Game Created',
+                    message: 'Lobby Created',
                     gameID: Number(generatedID)
                 }
                 console.log(JSON.stringify(response));
                 res.send(JSON.stringify(response));
             });
-        } //TODO: ELSE CREATE LOBBY FAILED
+        } else {
+            var response = {
+                status: 'failure',
+                error: 'You Already Made A Lobby'
+            }
+            console.log(JSON.stringify(response));
+            res.send(JSON.stringify(response));
+        }
     });
 }
 
+//GameID is a random 5 number int (11111-99999)
 function createGameID() {
     var gameID = "";
     var possibleChars = "123456789";
@@ -153,6 +167,219 @@ function createGameID() {
     }
     console.log("GameID: ", gameID);
     return gameID;
+}
+
+function lobbyInfoCheck(userID, res) {
+    var usersRef = database.ref("users");
+    usersRef.once("value").then(function (snapshot) {
+        if (snapshot.child(userID).exists() && snapshot.child(userID).child("signedIn").val() == 'true') {
+            var lobbies = [];
+            var gamesRef = database.ref("games");
+            gamesRef.once("value").then(function (snapshot) {
+                snapshot.forEach(function (childSnapshot) {
+                    var childData = childSnapshot.val();
+                    var lobby = {
+                        names: childData.child("names").val(),
+                        gameID: Number(childData.child("gameID").val()),
+                        isStarted: childData.child("isStarted").val(),
+                        partyLeader: childData.child("partyLeader").val(),
+                        stateID: Number(childData.child("stateID").val())
+                    }
+                    lobbies.push(lobby);
+                });
+                var response = {
+                    status: 'success',
+                    message: 'Current Lobbies',
+                    lobbies: lobbies
+                }
+                console.log(JSON.stringify(response));
+                res.send(JSON.stringify(response));
+            });
+        } else {
+            var response = {
+                status: 'failure',
+                error: 'User Not Signed In or User Does Not Exist'
+            }
+            console.log(JSON.stringify(response));
+            res.send(JSON.stringify(response));
+        }
+    });
+}
+
+function lobbyJoinCheck(userID, gameID, res){
+    var usersRef = database.ref("users");
+    usersRef.once("value").then(function (snapshot) {
+        if (snapshot.child(userID).exists() && snapshot.child(userID).child("signedIn").val() == 'true' &&
+            snapshot.child(userID).child("inLobby").val() == 'false') {
+            var gamesRef = database.ref("games");
+            gamesRef.once("value").then(function (snapshot) {
+                if (snapshot.child(gameID).exists() && snapshot.child(gameID).child("names").val().length <= 3) {
+                    var currentUserRef = database.ref("users/" + userID);
+                    var currentGameRef = database.ref("games/" + gameID);
+                    currentUserRef.update({
+                        inLobby: 'true'
+                    });
+                    var players = currentGameRef.child("players").val();
+                    players.push(userID);
+                    currentGameRef.update({
+                        stateID: currentUserRef.child("stateID").val() + 1,
+                        names: players
+                    });
+                    var response = {
+                        status: 'success',
+                        message: 'User Joined Lobby'
+                    }
+                    console.log(JSON.stringify(response));
+                    res.send(JSON.stringify(response));
+                } else if (snapshot.child(gameID).exists() && snapshot.child(gameID).child("names").val().length >= 4) {
+                    var response = {
+                        status: 'failure',
+                        error: 'Lobby Is Full'
+                    }
+                    console.log(JSON.stringify(response));
+                    res.send(JSON.stringify(response));
+                } else {
+                    var response = {
+                        status: 'failure',
+                        error: 'Lobby Does Not Exist'
+                    }
+                    console.log(JSON.stringify(response));
+                    res.send(JSON.stringify(response));
+                }
+            });
+        } else if (snapshot.child(userID).exists() && snapshot.child(userID).child("signedIn").val() == 'true' &&
+            snapshot.child(userID).child("inLobby").val() == 'true') {
+            var response = {
+                status: 'failure',
+                error: 'User Already In A Lobby'
+            }
+            console.log(JSON.stringify(response));
+            res.send(JSON.stringify(response));
+        } else {
+            var response = {
+                status: 'failure',
+                error: 'User Not Signed In or User Does Not Exist'
+            }
+            console.log(JSON.stringify(response));
+            res.send(JSON.stringify(response));
+        }
+    });
+}
+
+function lobbyLeaveCheck(userID, gameID, res) {
+    var usersRef = database.ref("users");
+    userRef.once("value").then(function (snapshot) {
+        if (snapshot.child(userID).exists() && snapshot.child(userID).child("signedIn").val() == 'true' &&
+            snapshot.child(userID).child("inLobby").val() == 'true') {
+            gamesRef = database.ref("games");
+            gamesRef.once("value").then(function (snapshot) {
+                if (gamesRef.child(gameID).exists() && gamesRef.child(gameID).child("players").val().indexOf(userID) > -1) {
+                    var currentUserRef = database.ref("users/" + userID);
+                    var currentGameRef = database.ref("games/" + gameID);
+                    currentUserRef.update({
+                        inLobby: 'false'
+                    });
+                    var players = currentGameRef.child("names").val();
+                    players = players.splice(players.indexOf(userID), 1);
+                    currentGameRef.update({
+                        stateID: currentGameRef.child("stateID").val() - 1,
+                        names: players
+                    });
+                    var response = {
+                        status: 'success',
+                        message: 'User Left Lobby'
+                    }
+                    console.log(JSON.stringify(response));
+                    res.send(JSON.stringify(response));
+                } else if (gamesRef.child(gameID).exists() && gamesRef.child(gameID).child("players").val().indexOf(userID) < 0) {
+                    var response = {
+                        status: 'failure',
+                        error: 'Lobby Does Not Contain This User'
+                    }
+                    console.log(JSON.stringify(response));
+                    res.send(JSON.stringify(response));
+                } else {
+                    var response = {
+                        status: 'failure',
+                        error: 'Lobby Does Not Exist'
+                    }
+                    console.log(JSON.stringify(response));
+                    res.send(JSON.stringify(response));
+                }
+            });
+        } else if (snapshot.child(userID).exists() && snapshot.child(userID).child("signedIn").val() == 'true' &&
+            snapshot.child(userID).child("inLobby").val() == 'false') {
+            var response = {
+                status: 'failure',
+                error: 'User Not In A Lobby'
+            }
+            console.log(JSON.stringify(response));
+            res.send(JSON.stringify(response));
+        } else {
+            var response = {
+                status: 'failure',
+                error: 'User Not Signed In or User Does Not Exist'
+            }
+            console.log(JSON.stringify(response));
+            res.send(JSON.stringify(response));
+        }
+    });
+}
+
+function startGameCheck(userID, gameID, res) {
+    userRef = database.ref("users");
+    usersRef.once("value").then(function (snapshot) {
+        if (snapshot.child(userID).exists() && snapshot.child(userID).child("signedIn").val() == 'true' &&
+            snapshot.child(userID).child("inLobby").val() == 'true') {
+            gamesRef = database.ref("games");
+            gamesRef.once(value).then(function (snapshot) {
+                if (gamesRef.child(gameID).child("partyLeader").val() == userID &&
+                    gamesRef.child(gameID).child("names").val().length == 4) {
+                    var currentGameRef = database.ref("games/" + gameID);
+                    currentGameRef.update({
+                        isStarted: true,
+                        stateID: currentGameRef.child("stateId").val() + 1
+                    });
+                    var response = {
+                        status: 'success',
+                        message: 'Game Start'
+                    }
+                    console.log(JSON.stringify(response));
+                    res.send(JSON.stringify(response));
+                } else if (gamesRef.child(gameID).child("partyLeader").val() == userID &&
+                    gamesRef.child(gameID).child("names").val().length != 4) {
+                    var response = {
+                        status: 'failure',
+                        error: 'Lobby Does Not Have 4 Players'
+                    }
+                    console.log(JSON.stringify(response));
+                    res.send(JSON.stringify(response));
+                } else {
+                    var response = {
+                        status: 'failure',
+                        error: 'User Not The Party Leader'
+                    }
+                    console.log(JSON.stringify(response));
+                    res.send(JSON.stringify(response));
+                }
+            })
+        } else if (snapshot.child(userID).exists() && snapshot.child(userID).child("signedIn").val() == 'true' &&
+            snapshot.child(userID).child("inLobby").val() == 'false') {
+            var response = {
+                status: 'failure',
+                error: 'User Not In A Lobby'
+            }
+            console.log(JSON.stringify(response));
+            res.send(JSON.stringify(response));
+        } else {
+            var response = {
+                status: 'failure',
+                error: 'User Not Signed In or User Does Not Exist'
+            }
+            console.log(JSON.stringify(response));
+            res.send(JSON.stringify(response));
+        }
+    });
 }
 
 app.use(express.static('public'))
@@ -193,13 +420,35 @@ app.post('/lobby/create', parser, function (req, res) {
     createLobbyCheck(userID, res);
 });
 
+//Display List of Lobbies
 app.post('/lobby/info', parser, function (req, res) {
+    console.log(req.body);
+    var userID = req.body.userID;
+    lobbyInfoCheck(userID, res);
 });
 
+//User Asking To Join A Lobby
 app.post('/lobby/join', parser, function (req, res) {
+    console.log(req.body);
+    var gameID = req.body.gameID;
+    var userID = req.body.userID;
+    lobbyJoinCheck(userID, gameID, res);
 });
 
+//User Asking To Leave A Lobby
 app.post('/lobby/leave', parser, function (req, res) {
+    console.log(req.body);
+    var gameID = req.body.gameID;
+    var userID = req.body.userID;
+    lobbyLeaveCheck(userID, gameID, res);
+});
+
+//Party Leader User Asking To Start Game
+app.post('/lobby/startgame', parser, function (req, res) {
+    console.log(req.body);
+    var gameID = req.body.gameID;
+    var userID = req.body.userID;
+    startGameCheck(userID, gameID, res);
 });
 
 app.all("/", (req, res) => {
